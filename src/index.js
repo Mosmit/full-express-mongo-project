92295ad9ab26c166e486d46d65d4a2bc47f8a24e
Mosmit <sirmosmit10@gmail.com>
2025-06-01 01:45:01 +0100

    created-search-button-functionality

--- src/index.js
+++ src/index.js
@@ -80,6 +80,29 @@ app.get("/news/:id", async (req, res) => {
     }
 }); 
 
+//to make the search button functional
+app.post('/news', async (req, res) => {
+    let search = req.body.search
+    try {
+        const newsAPI = await axios.get(`https://raddy.dev/wp-json/wp/v2/posts?search=${search}`)
+        res.render('newsSearch', { articles: newsAPI.data })
+    } catch (err) {
+        if (err.response) {
+            res.render('newsSearch', { articles: null })
+            console.log(err.response.data)
+            console.log(err.response.status)
+            console.log(err.response.headers)
+        } else if (err.requiest) {
+            res.render('newsSearch', { articles: null })
+            console.log(err.requiest)
+        } else {
+            res.render('newsSearch', { articles: null })
+            console.error('Error', err.message)
+        }
+    }
+});
+
+
 
 //register user
 app.post("/signup", async (req, res) =>{
